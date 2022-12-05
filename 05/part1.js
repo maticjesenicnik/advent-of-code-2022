c324d9caabcd5c3277feceb71e53ed4f60128b95
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-12-05 07:31:18 +0100

    add day 5 solution

---
+++ 05/part1.js
@@ -0,0 +1,41 @@
+let lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('input.txt')
+});
+
+let instructions = [];
+let crane = new Map();
+
+crane[1] = ['F', 'R', 'W'];
+crane[2] = ['P', 'W', 'V', 'D', 'C', 'M', 'H', 'T'];
+crane[3] = ['L', 'N', 'Z', 'M', 'P'];
+crane[4] = ['R', 'H', 'C', 'J'];
+crane[5] = ['B', 'T', 'Q', 'H', 'G', 'P', 'C'];
+crane[6] = ['Z', 'F', 'L', 'W', 'C', 'G'];
+crane[7] = ['C', 'G', 'J', 'Z', 'Q', 'L', 'V', 'W'];
+crane[8] = ['C', 'V', 'T', 'W', 'F', 'R', 'N', 'P'];
+crane[9] = ['V', 'S', 'R', 'G', 'H', 'W', 'J'];
+
+
+lineReader.on('line', function (line) {
+  if (line.startsWith('move'))
+    instructions.push(line);
+});
+
+lineReader.on('close', function () {
+  instructions.forEach(move => {
+    let input = move.split(' ');
+    let [quantity, from, to] = [+input[1], +input[3], +input[5]];
+
+    for (let i = 0; i < quantity; i++) {
+      let item = crane[from].shift();
+      crane[to].unshift(item);
+    }
+  });
+
+  let result = '';
+  for (let position in crane) {
+    result += crane[position].shift();
+  }
+
+  console.log(result);
+});
